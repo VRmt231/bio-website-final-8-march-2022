19ea8b847f6778a2add5f54488b01709baabdcf8
VeerMT <71698491+VRmt231@users.noreply.github.com>
2022-03-08 18:27:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function kakapo(){
+    window.location = "kakapo.html"
+}
+function thorny_dragon(){
+    window.location = "thorny_dragon.html"
+}
+function di_credits(){
+    window.location = "credits.html"
+}
---
+++ thorny_dragon.js
@@ -0,0 +1,6 @@
+function di_credits(){
+    window.location = 'credits.html'
+}
+function back(){
+window.location = 'index.html'
+}
